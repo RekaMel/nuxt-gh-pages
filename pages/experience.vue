907b386aa3f31b934c81c084cf995162755c8902
RekaMel <63839716+RekaMel@users.noreply.github.com>
2024-04-09 09:34:00 +0200

    pages

---
+++ pages/experience.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>My Coding Experience</h1>
+    <p>
+      Being new to Nuxt.js and Vue.js the task can have several challanges,
+      especially when tryin to transition from JS and React
+    </p>
+    <ul>
+      <li>
+        Setting up Nuxt project, configuring plugins and understanding modules
+        ex.installing composition API and Pinia and adding to the builtModules
+        or Modules in nuxt.config.js file
+      </li>
+      <li>Understanding how to pass data between components using props</li>
+      <li>Understnding Pinia and using a store outside a component</li>
+      <li>
+        Understanding Vue syntax, like v-model, Vue components and behaviour
+      </li>
+      <li>Installing fontawsome encountered conflicts of dependency</li>
+      <li>Deploying project on GIT Hub gh-pages</li>
+    </ul>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Form @formSubmitted="addCard" />
+    <div class="flex-container">
+      <Card v-for="(card, index) in cards" :key="index" :card="card" />
+    </div>
+    <nuxt-link to="/experience">Experience Page</nuxt-link>
+  </div>
+</template>
+
+<script>
+import Form from '~/components/Form.vue'
+import Card from '~/components/Card.vue'
+
+export default {
+  components: {
+    Form,
+    Card,
+  },
+  data() {
+    return {
+      cards: [],
+    }
+  },
+  methods: {
+    addCard(card) {
+      this.cards.push(card)
+    },
+  },
+}
+</script>
+
+<style>
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
