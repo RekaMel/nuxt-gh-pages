6f2c7bcb082f568027284f1dd4feb779162b6ba7
RekaMel <63839716+RekaMel@users.noreply.github.com>
2024-04-09 09:33:08 +0200

    components

---
+++ components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <div>{{ card.serviceName }}</div>
+    <div>{{ card.businessName }}</div>
+    <div>
+      <span
+        v-for="i in card.serviceRating"
+        :key="i"
+        class="fa-solid fa-star"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: Object,
+  },
+}
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
---
+++ components/Form.vue
@@ -0,0 +1,53 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="name">Service Name </label>
+    <input
+      v-model="serviceName"
+      type="text"
+      placeholder="Type Service Name"
+      id="name"
+    />
+    <label for="business">Business Name</label>
+    <input
+      v-model="businessName"
+      type="text"
+      placeholder="Type Business Name"
+      id="business"
+    />
+    <label for="rating">Rate Service</label>
+    <input
+      v-model.number="serviceRating"
+      type="number"
+      placeholder="Service Rating"
+      id="rating"
+    />
+    <button type="submit">Details</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      serviceName: '',
+      businessName: '',
+      serviceRating: null,
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('formSubmitted', {
+        serviceName: this.serviceName,
+        businessName: this.businessName,
+        serviceRating: this.serviceRating,
+      })
+      this.resetForm()
+    },
+    resetForm() {
+      this.serviceName = ''
+      this.businessName = ''
+      this.serviceRating = null
+    },
+  },
+}
+</script>
